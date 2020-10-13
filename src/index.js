f07edde4c5635d73c77ef10544b52b07484f38be
ext_vvoevodkina <vivoe@yandex.ru>
2020-10-13 10:20:07 +0300

    feat: towel sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let res = [];
+  let inversion = false;
+  if (matrix) throuthArray(matrix);
+
+  function throuthArray(matrix) {
+    matrix = (inversion) ? matrix.reverse() : matrix;
+    matrix.forEach(function(el, ind){
+      if (Array.isArray(el)) {
+        inversion = (ind%2) ? true : false
+        throuthArray(el)
+      } else {
+        res.push(el)
+      }
+    })
+  }
+
+  return res;
 }
